93edc7d6068e5ab2533465c5ea04baa5435791a5
sdutt <shatyaki.dutt@hotmail.com>
2025-10-16 14:24:32 +0530

    fix batch writing

--- cmd/fanout/client.go
+++ cmd/fanout/client.go
@@ -85,21 +85,20 @@ func (c *Client) writePump() {
 				return
 			}
 
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
+			// Send each message as a separate WebSocket frame
+			// This ensures frontend can parse each JSON object individually
+			if err := c.conn.WriteMessage(websocket.TextMessage, message); err != nil {
 				return
 			}
-			w.Write(message)
 
-			// Add queued messages to the current websocket message
+			// Send any queued messages as separate frames
+			// Don't batch them together to avoid JSON parsing issues
 			n := len(c.send)
 			for i := 0; i < n; i++ {
-				w.Write([]byte{'\n'})
-				w.Write(<-c.send)
-			}
-
-			if err := w.Close(); err != nil {
-				return
+				c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+				if err := c.conn.WriteMessage(websocket.TextMessage, <-c.send); err != nil {
+					return
+				}
 			}
 
 		case <-ticker.C:
